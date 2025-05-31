77a0806a722e74d4b128f10f8bd6e2b78c13219f
Zhenya <sunak.zhenya@lll.kpi.ua>
2025-05-31 23:56:19 +0300

    add insert method

--- linked_list.py
+++ linked_list.py
@@ -30,3 +30,33 @@ class DoublyLinkedList:
         for _ in range(index):
             current = current.next
         return current.data
+
+
+    def insert(self, element, index):
+        if index < 0 or index > self._length:
+            raise IndexError("Invalid index")
+
+        new_node = Node(element)
+
+        if index == 0:  # вставка на початок
+            new_node.next = self.head
+            if self.head:
+                self.head.prev = new_node
+            self.head = new_node
+            if self._length == 0:
+                self.tail = new_node
+        elif index == self._length:  # вставка в кінець (аналог append)
+            self.append(element)
+            return
+        else:  # вставка в середину
+            current = self.head
+            for _ in range(index):
+                current = current.next
+            prev_node = current.prev
+            prev_node.next = new_node
+            new_node.prev = prev_node
+            new_node.next = current
+            current.prev = new_node
+
+        self._length += 1
+
