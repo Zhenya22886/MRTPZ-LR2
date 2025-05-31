b66d8edc6e640764876c032a6c0a2bee892b1665
Zhenya <sunak.zhenya@lll.kpi.ua>
2025-05-31 18:03:50 +0300

    add tests for insert and delete methods

--- test_array_list.py
+++ test_array_list.py
@@ -13,3 +13,26 @@ class TestArrayList(unittest.TestCase):
 
 if __name__ == '__main__':
     unittest.main()
+
+    def test_insert_valid_and_invalid(self):
+        lst = ArrayList()
+        lst.append('a')
+        lst.insert('b', 1)  # insert at end
+        lst.insert('c', 0)  # insert at beginning
+        self.assertEqual(lst.get(0), 'c')
+        self.assertEqual(lst.get(1), 'a')
+        self.assertEqual(lst.get(2), 'b')
+        with self.assertRaises(IndexError):
+            lst.insert('d', -1)
+        with self.assertRaises(IndexError):
+            lst.insert('e', 10)
+
+    def test_delete_valid_and_invalid(self):
+        lst = ArrayList()
+        lst.append('x')
+        lst.append('y')
+        deleted = lst.delete(0)
+        self.assertEqual(deleted, 'x')
+        self.assertEqual(lst.length(), 1)
+        with self.assertRaises(IndexError):
+            lst.delete(5)
