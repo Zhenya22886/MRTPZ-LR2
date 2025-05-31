844ac76a0d7df6ba0d0e2b73cbdad42b3f76d63d
Zhenya <sunak.zhenya@lll.kpi.ua>
2025-05-31 16:39:49 +0300

    add insert and get methods

--- array_list.py
+++ array_list.py
@@ -7,3 +7,13 @@ class ArrayList:
 
     def append(self, element):
         self.data.append(element)
+
+	def insert(self, element, index):
+        if index < 0 or index > len(self.data):
+            raise IndexError("Invalid index")
+        self.data.insert(index, element)
+
+    def get(self, index):
+        if index < 0 or index >= len(self.data):
+            raise IndexError("Invalid index")
+        return self.data[index]
